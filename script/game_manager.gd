4e14ad37558736f631cc3a3b9029cd8b46e697a2
alandarryl <okanajonathan2@gmail.com>
2026-07-16 16:22:04 +0200

    add a score label

--- script/game_manager.gd
+++ script/game_manager.gd
@@ -2,7 +2,12 @@ extends Node
 
 var score = 0
 @onready var label = $Label
+@onready var score_label: Label = %ScoreLabel
+
+func _ready() -> void:
+	score_label.text = "Welcome you have: " + str(score)
 
 func add_coin():
 	score +=1
 	label.text = "Your score is " + str(score) + " !"
+	score_label.text = "You have : " + str(score)
